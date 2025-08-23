dd1d41684a9b1cdd210f63830ac61837a6e2eed6
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-23 00:23:12 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Manifest */}
+        {/* Manifest for PWA */}
         <link rel="manifest" href="/manifest.json" />
         {/* Favicon */}
         <link rel="icon" href="/gas-logo.png" />
-        {/* Theme color */}
+        {/* Theme color for mobile browsers */}
         <meta name="theme-color" content="#005826" />
-        {/* Mobile / Apple fallback */}
+        {/* Enable standalone on mobile */}
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="GAS CMS" />
