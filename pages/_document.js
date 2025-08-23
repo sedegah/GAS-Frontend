955d1c11f01f881580730f9f8186bc5edc119b6d
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-23 00:37:10 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Manifest for PWA */}
-        <link rel="manifest" href="/manifest.json" />
+        {/* Manifest */}
+        <link rel="manifest" href="/manifest.json" /> 
+        
         {/* Favicon */}
-        <link rel="icon" href="/gas-logo.png" />
-        {/* Theme color for mobile browsers */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        {/* Theme color */}
         <meta name="theme-color" content="#005826" />
-        {/* Enable standalone on mobile */}
+
+        {/* Mobile / Apple fallback */}
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="GAS CMS" />
-        <link rel="apple-touch-icon" href="/gas-logo.png" />
       </Head>
       <body>
         <Main />
