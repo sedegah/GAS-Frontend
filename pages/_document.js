e4bb2f7dc4b769dba6111b0ca29df02dedc1081a
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-23 01:07:01 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,19 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Manifest */}
-        <link rel="manifest" href="/manifest.json" /> 
-        
-        {/* Favicon */}
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-
-        {/* Theme color */}
         <meta name="theme-color" content="#005826" />
-
-        {/* Mobile / Apple fallback */}
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="GAS CMS" />
