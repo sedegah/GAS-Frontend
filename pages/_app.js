a16d34726ccfc38656f8ec71cd19f0f8da849bc9
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-23 00:40:57 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .then(() => console.log('Service Worker registered'))
+        .catch((err) => console.log('Service Worker registration failed', err));
+    }
+  }, []);
+
   return (
     <>
       <Head>
